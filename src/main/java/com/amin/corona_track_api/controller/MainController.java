41c3bc13c30872076698b10fe4520080e183b6de
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-03-31 20:36:40 -0500

    Version Tag fixed

--- src/main/java/com/amin/corona_track_api/controller/MainController.java
+++ src/main/java/com/amin/corona_track_api/controller/MainController.java
@@ -2,7 +2,6 @@ package com.amin.corona_track_api.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -10,7 +9,7 @@ import java.util.Map;
 
 @RestController
 public class MainController {
-    @Value("${app.version")
+    @Value("${app.version}")
     private String appVersion;
 
     @GetMapping("/")
--- src/main/java/com/amin/corona_track_api/service/impl/MainDataServiceImpl.java
+++ src/main/java/com/amin/corona_track_api/service/impl/MainDataServiceImpl.java
@@ -32,9 +32,18 @@ public class MainDataServiceImpl implements MainDataService {
         StringReader csvBodyReader = new StringReader(response.getBody());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
+
+
             LocationData locationStat = new LocationData();
-            locationStat.setState(record.get("Province/State"));
-            locationStat.setCountry(record.get("Country/Region"));
+
+            System.out.println(record.get("Province/State"));
+            System.out.println(record.get("Country/Region"));
+
+            //String state = (record.get("Province/State").isEmpty()) ? " " : record.get("Province/State") ;
+            String country = (record.get("Country/Region").isEmpty()) ? " " : record.get("Country/Region");
+
+            locationStat.setState("");
+            locationStat.setCountry(country);
 
             //get last Date
             String lastDay = record.get(record.size() - 1);
