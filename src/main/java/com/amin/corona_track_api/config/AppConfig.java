bd3532a7e30efe62cb5998dc215675b8cfb8b30a
Amin <amin030296@gmail.com>
2020-04-14 11:50:30 -0500

    cloud-build

--- src/main/java/com/amin/corona_track_api/config/AppConfig.java
+++ src/main/java/com/amin/corona_track_api/config/AppConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
 @Import(SwaggerConfig.class)
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     RestTemplate restTemplate(){
@@ -20,5 +22,11 @@ public class AppConfig {
     @Bean
     public Filter moesifFilter() {
         return new MoesifFilter("eyJhcHAiOiIzNDU6NDQ4IiwidmVyIjoiMi4wIiwib3JnIjoiODg6MjEzIiwiaWF0IjoxNTgzOTcxMjAwfQ.SjxLJq8Qb8WgwBssPXh47SwPTO-2R8dvtm8-pGdiiLQ");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
